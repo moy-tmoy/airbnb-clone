a8015d506fd8df57e4d25121817d0837042aa496
Ishoboy <francislandia@icloud.com>
2022-10-15 22:03:25 +0800

    done coding the bookings api, off to start coding on login and signup page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
+    render 'home'
+  end
+
+  def property
+    @data = { property_id: params[:id] }.to_json
+    render 'property'
   end
 end
+
