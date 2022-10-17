7fa4ed3c55a61933c8cdc391db00b67f7dd2bb76
Ishoboy <francislandia@icloud.com>
2022-10-17 23:13:21 +0800

    done building the bookings front-end transferred the property files to its separate folder the react dates and its date range picker is all working with its built in methods that I use, all good so far

--- app/javascript/src/property/property.jsx
+++ app/javascript/src/property/property.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import Layout from "@src/layout";
+import BookingWidget from './bookingWidget';
 import { handleErrors } from "@utils/fetchHelper";
 
 import "./property.scss"
@@ -67,6 +67,9 @@ class Property extends React.Component {
                             <hr/>
                             <p>{description}</p>
                         </div>
+                        <div className="cil-12 col-lg-5">
+                            <BookingWidget property_id={id} price_per_night={price_per_night} />
+                        </div>
                     </div>
                 </div>
             </Layout>
@@ -74,12 +77,4 @@ class Property extends React.Component {
     }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    const node = document.getElementById('params');
-    const data = JSON.parse(node.getAttribute('data-params'));
-
-    ReactDOM.render(
-        <Property property_id={data.property_id}/>,
-        document.body.appendChild(document.createElement('div')),
-    )
-})
+export default Property;
